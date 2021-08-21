17dfb5fedf1af7896b363a29b20e1bce52b7cbed
Debora <debvm@hotmail.com>
2021-08-21 17:37:28 -0300

    começo de estilização

--- src/App.js
+++ src/App.js
@@ -1,32 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import MostPlayed from './components/MostPlayed/MostPlayed';
-import Favorites from './components/Favorites/Favorites';
+import logo from "./logo.svg";
+import "./App.css";
+import api from "./server/api";
+import React, { useEffect, useState } from "react";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import IconButton from "@material-ui/core/IconButton";
+import PlayArrowIcon from "@material-ui/icons/PlayArrow";
+import StarIcon from "@material-ui/icons/Star";
+import SearchIcon from '@material-ui/icons/Search';
+import InputBase from '@material-ui/core/InputBase';
+//import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+//import MostPlayed from "./components/MostPlayed/MostPlayed";
+//import Favorites from "./components/Favorites/Favorites";
 
-function App() {
+export default function App() {
+  const [music, setMusic] = useState();
+
+  useEffect(() => {
+    api
+      .get("/radio/37151/tracks")
+      .then((response) => setMusic(response.data))
+      .catch((err) => {
+        console.error("ops! ocorreu um erro" + err);
+      });
+  }, []);
+
   return (
     <div className="App">
+      
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <input type= "search" className = "MuiInputBase-inputTypeSearch" placeholder = "Pesquisa"></input>
+        <InputBase
+          type="search"
+          className="MuiInputBase-inputTypeSearch"
+          placeholder="Pesquisar"
+          />
+        <IconButton type="submit" className="busca" aria-label="search">
+        <SearchIcon />
+      </IconButton>
       </header>
-      <Router basename={process.env.PUBLIC_URL}>
-
-          <Switch>
-
-
-            <Route exact path='/' component={MostPlayed} />
-            <Route exact path='/musicas-favoritas' component={Favorites} />
-            <Route path='*'>
-              <h1>Página não encontrada /: </h1>
-            </Route>
-
-
-          </Switch>
-      </Router>
+      
     </div>
   );
 }
-
-export default App;
--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,29 +1,30 @@
-
-import api from "./server/api";
 import React, { useEffect, useState } from "react";
-import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
-import IconButton from "@material-ui/core/IconButton";
-import PlayArrowIcon from "@material-ui/icons/PlayArrow";
-import StarIcon from "@material-ui/icons/Star";
+import api from "./server/api";
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import IconButton from '@material-ui/core/IconButton';
+import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+import StarIcon from '@material-ui/icons/Star';
 
-function MostPlayed() {
+
+
+export default function MediaControlCard() {
   const [music, setMusic] = useState();
+  const search = InputEvent
 
   useEffect(() => {
     api
-      .get("/radio/37151/tracks")
+      .get(`/search?q=${search}`)
       .then((response) => setMusic(response.data))
       .catch((err) => {
         console.error("ops! ocorreu um erro" + err);
       });
   }, []);
 
-    return (
-      <div className="pop">
-        <h1>Mais Populares</h1>
-      <div className="container">
-      <Card className="root">
+  return (
+    <div className="search">
+    <h1>Busca por {search}</h1>
+    <Card className="root">
         <div className="details">
           <CardContent className="content">
           <img classeName="cover" alt="capa">
@@ -43,9 +44,6 @@ function MostPlayed() {
           </div>
         </div>
       </Card>
-      </div>
-      </div>
-    );
-  }
-  
-  export default MostPlayed;
+    </div>
+  );
+}
